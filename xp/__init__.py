fed1b74345dcb01965da7e38b5a77bfa43cf16f4
Quentin Roy <quentin@quentinroy.fr>
2013-08-19 22:06:17 +0200

    current_trial & beginning of the loger

--- xp/__init__.py
+++
@@ -1,9 +0,0 @@
-from run import Run
-from trial import Trial
-from xp import Xp
-from block import Block
-
-def _parse_values(values_string):
-    value_strings = values_string.split(',')
-    value_seq = (value_string.split('=', 1) for value_string in value_strings if value_string != "")
-    return {key: value for (key, value) in value_seq}
--- xpserver.py
+++ xpserver.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from xp import Xp
 import json
+import os
 
+
+dir = os.path.dirname(__file__)
 app = Flask(__name__)
-xp = Xp("/Users/quentin/Workspace/PyCharm/Test/nomodexp.xml")
+xp = Xp(os.path.join(dir, "nomodexp.xml"), os.path.join(dir, "logs"))
 
 
 @app.route('/available_runs')
@@ -11,19 +14,31 @@ def available_runs():
     unstarted_runs = [run.id for run in xp.iter_runs() if not run.started()]
     return json.dumps(unstarted_runs)
 
+
 @app.route('/uncompleted_runs')
 def uncompleted_runs():
-    uncompleted_runs = [run.id for run in xp.inter_runs() if run.started() and not run.completed()]
+    uncompleted_runs = [run.id for run in xp.iter_runs() if run.started() and not run.completed()]
     return json.dumps(uncompleted_runs)
+
 
 @app.route('/xp_id')
 def xp_id():
-    return xp.id()
+    return xp.id
 
 
-@app.route('/next_trial/<run_id>')
-def next_trial(run_id):
-    run = xp.run(run_id)
+@app.route('/<run_id>/current_trial')
+def current_trial(run_id):
+    trial = xp.get_run(run_id).current_trial()
+    values = trial.values()
+    values.update({
+        "trial_num": trial.num,
+        "block_num": trial.block.num,
+        "run_id": trial.run.id,
+        "xp_id": trial.xp.id,
+        "trial_count": len(trial.block),
+        "block_count": trial.run.block_count()
+    })
+    return json.dumps(values)
 
 
 if __name__ == '__main__':
