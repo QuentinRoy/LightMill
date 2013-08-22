4da664b17b46e2d79b6dce3648b0218a73aec00f
Quentin Roy <quentin@quentinroy.fr>
2013-08-22 17:27:13 +0200

    Relationship cascade in the model. Trial and block number automation. Some more test.

--- expserver/model.py
+++ expserver/model.py
@@ -13,7 +13,10 @@ class Experiment(db.Model):
     author = db.Column(db.String(200), nullable=True)
     description = db.Column(db.Text, nullable=True)
 
-    factors = db.relationship('Factor', backref='experiment')
+    factors = db.relationship('Factor',
+                              backref=db.backref('experiment', lazy='joined'),
+                              lazy='dynamic',
+                              cascade="all, delete-orphan")
 
     def __init__(self, id, name, factors, author=None, description=None):
         self.name = name
@@ -26,6 +29,9 @@ class Experiment(db.Model):
         return "<{} {} (name: '{}')>" \
             .format(self.__class__.__name__, self.id, self.name)
 
+    def get_factor(self, factor_id):
+        return self.factors.filter_by(id=factor_id).one()
+
 
 class Run(db.Model):
     _db_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -33,7 +39,11 @@ class Run(db.Model):
 
     id = db.Column(db.String(10))
 
-    experiment = db.relationship(Experiment, backref='runs')
+    experiment = db.relationship(Experiment,
+                                 backref=db.backref('runs',
+                                                    lazy='dynamic',
+                                                    cascade="all, delete-orphan"),
+                                 lazy='joined')
 
     __table_args__ = (
         db.UniqueConstraint("_experiment_db_id", "id"),
@@ -46,7 +56,19 @@ class Run(db.Model):
 
     def __repr__(self):
         return '<{} {} (experiment id: {})>' \
-            .format(self.__class__.__name__, self.id, self.experiment.id)
+            .format(self.__class__.__name__,
+                    self.id,
+                    self.experiment.id if self.experiment else None)
+
+
+def _free_number(number_list):
+    numbers = sorted(number_list)
+    i = 0
+    for number in numbers:
+        if number != i:
+            return i
+        i += 1
+    return i
 
 
 class Block(db.Model):
@@ -54,7 +76,11 @@ class Block(db.Model):
 
     _run_db_id = db.Column(db.Integer, db.ForeignKey(Run._db_id), nullable=False)
 
-    run = db.relationship(Run, backref='blocks')
+    run = db.relationship(Run,
+                          backref=db.backref('blocks',
+                                             lazy='dynamic',
+                                             cascade="all, delete-orphan"),
+                          lazy='joined')
 
     number = db.Column(db.Integer, nullable=False)
     practice = db.Column(db.Boolean)
@@ -63,14 +89,17 @@ class Block(db.Model):
         db.UniqueConstraint("_run_db_id", "number"),
     )
 
-    def __init__(self, number, run, practice=False):
-        self.number = number
-        self.run = run
+    def __init__(self, run, number=None, practice=False):
         self.practice = practice
+        self.number = number if number is not None else _free_number(block.number for block in run.blocks)
+        self.run = run
 
     def __repr__(self):
         return '<{} {} (run id: {}, experiment id: {})>' \
-            .format(self.__class__.__name__, self.num, self.run.id, self.run.experiment.id)
+            .format(self.__class__.__name__,
+                    self.number,
+                    self.run.id if self.run is not None else None,
+                    self.run.experiment.id if self.run is not None and self.run.experiment is not None else None)
 
 
 class Trial(db.Model):
@@ -78,7 +107,11 @@ class Trial(db.Model):
 
     _block_db_id = db.Column(db.Integer, db.ForeignKey(Block._db_id), nullable=False)
 
-    block = db.relationship(Block, backref='trials')
+    block = db.relationship(Block,
+                            backref=db.backref('trials',
+                                               lazy='dynamic',
+                                               cascade="all, delete-orphan"),
+                            lazy='joined')
 
     number = db.Column(db.Integer, nullable=False)
     completed = db.Column(db.Boolean, default=False)
@@ -87,18 +120,19 @@ class Trial(db.Model):
         db.UniqueConstraint("number", "_block_db_id"),
     )
 
-    def __init__(self, number, block, experiment=None):
-        self.number = number
+    def __init__(self, block, number=None):
+        self.number = number if number is not None else _free_number(trial.number for trial in block.trials)
         self.block = block
-        self.experiment = experiment
 
     def __repr__(self):
         return '<{} {} (block number: {}, run id: {}, experiment id: {}, completed: {})>' \
             .format(self.__class__.__name__,
                     self.number,
-                    self.block.number,
-                    self.block.run.id,
-                    self.block.run.experiment.id,
+                    self.block.number if self.block else None,
+                    self.block.run.id if self.block is not None and self.block.run is not None else None,
+                    self.block.run.experiment.id if (self.block is not None and
+                                                     self.block.run is not None and
+                                                     self.block.run.experiment is not None) else None,
                     self.completed)
 
 
@@ -108,17 +142,20 @@ class Factor(db.Model):
 
     id = db.Column(db.String(40), nullable=False)
     name = db.Column(db.String(200))
-    type = db.Column(db.String(80))
+    type = db.Column(db.String(40))
     kind = db.Column(db.String(20))
     tag = db.Column(db.String(80))
 
-    values = db.relationship('FactorValue', backref="factor")
+    values = db.relationship('FactorValue',
+                             backref=db.backref('factor', lazy='joined'),
+                             lazy='joined',
+                             cascade="all, delete-orphan")
 
     __table_args__ = (
         db.UniqueConstraint("id", "_experiment_db_id"),
     )
 
-    def __init__(self, id, values, name=None, type=None, kind=None, tag=None):
+    def __init__(self, id, values, type, name=None, kind=None, tag=None):
         self.id = id
         self.name = name
         self.type = type
@@ -131,7 +168,7 @@ class Factor(db.Model):
             .format(self.__class__.__name__,
                     self.id,
                     self.name,
-                    self.experiment.id,
+                    self.experiment.id if self.experiment is not None else None,
                     self.type,
                     self.kind,
                     self.tag)
@@ -139,7 +176,7 @@ class Factor(db.Model):
 
 class FactorValue(db.Model):
     _db_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    _factor_db_id = db.Column(db.String(40), db.ForeignKey(Factor._db_id))
+    _factor_db_id = db.Column(db.String(40), db.ForeignKey(Factor._db_id), nullable=False)
 
     id = db.Column(db.String(10), nullable=False)
     name = db.Column(db.String(200))
@@ -154,17 +191,23 @@ class FactorValue(db.Model):
         self.name = name
 
     def __repr__(self):
-        return "<{class} {id} (name: '{name}', factor id: {factor}".format({
-            "id": self.id,
-            "name": self.name,
-            "class": self.__class__.__name__,
-            "factor": self.factor.id
-        })
+        return "<{class_name} {id} (name: '{name}', factor id: {factor}>".format(
+            id=self.id,
+            name=self.name,
+            class_name=self.__class__.__name__,
+            factor=self.factor.id if self.factor is not None else None
+        )
 
+
+#################################
+#                               #
+#           TEST                #
+#                               #
+#################################
 
 if __name__ == '__main__':
     import os
-    from random import randint
+    import random
 
     db_uri = os.path.abspath(os.path.join(os.path.dirname(__name__), '../model_test.db'))
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + db_uri
@@ -176,8 +219,10 @@ if __name__ == '__main__':
     def gen_factors():
         for f_num in range(3):
             values = list(gen_factor_values())
-            yield Factor('f{}'.format(f_num), values, 'Test Factor number {}'.format(f_num))
-
+            yield Factor(id='f{}'.format(f_num),
+                         values=values,
+                         type=random.choice(('Integer', 'String')),
+                         name='Test Factor number {}'.format(f_num))
 
     def create_objs():
 
@@ -187,17 +232,19 @@ if __name__ == '__main__':
                              'Test Experiment number {}'.format(exp_num),
                              factors=list(gen_factors()))
 
-            factor_sup = Factor('fsup', name='Factor created after the experiment', values=list(gen_factor_values()))
+            factor_sup = Factor('fsup',
+                                type='Integer',
+                                name='Factor created after the experiment',
+                                values=list(gen_factor_values()))
             exp.factors.append(factor_sup)
 
-            for run_num in range(4):
+            for run_num in range(3):
                 run = Run('S' + str(run_num), exp)
-                for block in (Block(i, run) for i in range(3)):
-                    db.session.add(block)
-                    for trial in (Trial(i, block) for i in range(10)):
-                        db.session.add(trial)
+                for _ in range(3):
+                    block = Block(run)
+                    for _ in range(10):
+                        Trial(block)
             db.session.add(exp)
-            db.session.add(run)
         db.session.commit()
 
     def read():
@@ -211,6 +258,8 @@ if __name__ == '__main__':
                 for block in run.blocks:
                     for trial in block.trials:
                         print('  ' + repr(trial))
+
+            print('get factor f1: ' + repr(exp.get_factor('f1')))
 
 
     if os.path.exists(db_uri):
