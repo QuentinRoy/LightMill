eab46e6755fe5ac3d3a54ca532188f04eb010e7b
Quentin Roy <quentin.roy@ge.com>
2013-11-22 17:52:43 +0100

    Fix the csv export script bug when a factor and a measure have the same name (or have the name of a header).

--- expserver/getcsv.py
+++ expserver/getcsv.py
@@ -6,7 +6,7 @@ import os
 from csv import DictWriter
 
 
-def get_trial_dict_row(trial):
+def get_trial_dict_row(trial, fieldnames):
     print('  Trial {} of block {}'.format(trial.number, trial.block.number))
     row = {
         'Experiment Id': trial.run.experiment.id,
@@ -19,10 +19,14 @@ def get_trial_dict_row(trial):
         factor = factor_value.factor
         factor_name = factor.name or factor.id
         value_name = factor_value.name or factor_value.id
+        if factor_name not in fieldnames:
+            factor_name += ' (factor)'
         row[factor_name] = convert_bool(value_name)
     for measure_value in trial.measure_values:
         measure = measure_value.measure
         measure_name = measure.name or measure.id
+        if measure_name not in fieldnames:
+            measure_name += ' (measure)'
         row[measure_name] = convert_bool(measure_value.value)
     return row
 
@@ -37,26 +41,41 @@ def convert_bool(val):
 
 
 def create_logger(experiment, target_path):
-    fieldnames = ['Experiment Id', 'Run Id', 'Block Number', 'Trial Number', 'Practice']
+    header_names = ['Experiment Id', 'Run Id', 'Block Number', 'Trial Number', 'Practice']
+    factor_names = []
+    measure_names = []
 
     for factor in sorted(experiment.factors, key=lambda x: x.id):
-        fieldnames.append(factor.name or factor.id)
+        factor_name = factor.name or factor.id
+        if factor_name in header_names:
+            factor_name += ' (factor)'
+        factor_names.append(factor_name)
 
     for measure in sorted(experiment.measures.itervalues(), key=lambda x: x.id):
         if measure.trial_level:
-            fieldnames.append(measure.name or measure.id)
+            measure_name = measure.name or measure.id
+            # if there is a factor with the same name
+            for index, factor_name in enumerate(factor_names):
+                if factor_name == measure_name:
+                    measure_name += ' (measure)'
+                    factor_names[index] = factor_name+' (factor)'
+                if factor_name == measure_name + ' (factor)':
+                    measure_name += ' (measure)'
+            measure_names.append(measure_name)
+
+    field_names = header_names + factor_names + measure_names
 
     target_file = open(target_path, 'w')
-    dict_writer = DictWriter(target_file, fieldnames)
+    dict_writer = DictWriter(target_file, field_names)
     dict_writer.writeheader()
-    return dict_writer
+    return field_names, dict_writer
 
 
-def run_csv_export(run, logger):
+def run_csv_export(run, logger, fieldnames):
     print('Export Run {}:'.format(run.id))
 
     for trial in run.trials:
-        row = get_trial_dict_row(trial)
+        row = get_trial_dict_row(trial, fieldnames)
         logger.writerow(row)
 
 
@@ -70,11 +89,11 @@ class MultiLogger:
 
 
 def xp_csv_export(experiment, target_dir):
-    xp_logger = create_logger(experiment, target_dir+'.csv')
+    fieldnames, xp_logger = create_logger(experiment, target_dir+'.csv')
     for run in experiment.runs:
         if run.completed():
-            run_logger = create_logger(experiment, os.path.join(target_dir, run.id + '.csv'))
-            run_csv_export(run, MultiLogger([xp_logger, run_logger]))
+            _, run_logger = create_logger(experiment, os.path.join(target_dir, run.id + '.csv'))
+            run_csv_export(run, MultiLogger([xp_logger, run_logger]), fieldnames)
 
 
 def csv_export(target_dir):
